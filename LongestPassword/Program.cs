611eb72c8f1a987c0c5165b0b66e08507ac47c93
Anton <anton.abyzov@gmail.com>
2017-08-09 01:17:10 +0300

    solitaire DP

---
+++ LongestPassword/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+// you can also use other imports, for example:
+// using System.Collections.Generic;
+
+// you can write to stdout for debugging purposes, e.g.
+// Console.WriteLine("this is a debug message");
+
+class Solution
+{
+    public int solution(string S)
+    {
+        if (S.Length == 0)
+            return -1;
+        var words = S.Split(' ');
+        var longestPass = -1;
+        foreach (var word in words)
+        {
+            if (IsValidPassword(word))
+            {
+                longestPass = Math.Max(longestPass, word.Length);
+            }
+        }
+        return longestPass;
+    }
+
+    private bool IsValidPassword(string word)
+    {
+        var charCount = 0;
+        var digitCount = 0;
+        for (int i = 0; i < word.Length; i++)
+        {
+            if (!Char.IsDigit(word[i]) && !((word[i] >= 'A' && word[i] <= 'Z') || (word[i] >= 'a' && word[i] <= 'z')))
+                return false;
+            if ((word[i] >= 'A' && word[i] <= 'Z') || (word[i] >= 'a' && word[i] <= 'z'))
+                charCount++;
+            if (char.IsDigit(word[i]))
+                digitCount++;
+        }
+        return charCount % 2 == 0 && digitCount % 2 != 0;
+    }
+
+    public static void Main()
+    {
+        var str = Console.ReadLine();
+        Console.WriteLine(new Solution().solution(str));
+    }
+}
---
+++ NumberSolitaire/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace NumberSolitaire
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var startPos = 0;
+            var line = Console.ReadLine().Split(' ');
+            var a = Array.ConvertAll(line, int.Parse);
+            var solution = new Program().solution(a);
+            Console.WriteLine(solution);
+        }
+
+        //public int solution(int[] A)
+        //{
+        //    var startPos = 0;
+        //    var sum = A[0];
+        //    var i = 0;
+        //    while(i<A.Length)
+        //    {
+        //        var maxMove = int.MinValue;
+        //        int j;
+        //        var indexOfMaxMove = 0;
+        //        for (j = 1; j <= 6; j++)
+        //        {
+        //            if (i + j >= A.Length) continue;
+        //            if (A[i + j] > maxMove)
+        //            {
+        //                maxMove = A[i + j];
+        //                indexOfMaxMove = j;
+        //            }
+        //        }
+        //        sum += maxMove;
+        //        i += indexOfMaxMove;
+        //    }
+        //    return sum;
+        //}
+
+        public int solution(int[] a)
+        {
+            var maxInt = new int[a.Length];
+            maxInt[0] = a[0];
+            maxInt[1] = a[0] + a[1];
+
+            for (int i = 2; i < a.Length; i++)
+            {
+                int max = Int32.MinValue;
+                for (int j = 1; j <= 6; j++)
+                {
+                    if (i - j >= 0)
+                    {
+                        max = Math.Max(max, maxInt[i - j] + a[i]);
+                    }
+                }
+                maxInt[i] = max;
+            }
+            return maxInt[a.Length - 1];
+        }
+    }
+}
