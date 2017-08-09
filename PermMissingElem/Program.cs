fc96f0c088bb96b1100b6babf471a5abc79ca08c
Anton <anton.abyzov@gmail.com>
2017-08-10 00:38:46 +0300

    permMissingElem

---
+++ PermMissingElem/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        var arr = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+        var result = new Solution().solution(arr);
+        Console.WriteLine(result);
+    }
+
+    public int solution(int[] A)
+    {
+        if (A.Length == 0)
+            return 1;
+
+        var visited = new HashSet<int>();
+        var optimal = new HashSet<int>();
+        for (int i = 1; i <= A.Length + 1; i++)
+        {
+            optimal.Add(i);
+        }
+        for (int i = 0; i < A.Length; i++)
+        {
+            visited.Add(A[i]);
+        }
+        optimal.ExceptWith(visited);
+        return optimal.First();
+    }
+}
