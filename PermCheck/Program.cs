992c228ba72f60039e1079656614b27e81dff1d9
Anton <anton.abyzov@gmail.com>
2017-08-10 01:29:56 +0300

    permcheck

---
+++ PermCheck/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+// you can also use other imports, for example:
+// using System.Collections.Generic;
+
+// you can write to stdout for debugging purposes, e.g.
+// Console.WriteLine("this is a debug message");
+
+class Solution
+{
+    public int solution(int[] A)
+    {
+        var set = new HashSet<int>(A);
+        if (A.Length != set.Count)
+            return 0;
+        for (int i = 1; i <= A.Length; i++)
+        {
+            if (!set.Contains(i))
+                return 0;
+        }
+        return 1;
+    }
+
+    public static void Main()
+    {
+        var arr = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+        var result = new Solution().solution(arr);
+        Console.WriteLine(result);
+    }
+}
