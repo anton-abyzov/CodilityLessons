7ef06df48719d2d858adfbef53c5be5258e1755e
Anton <anton.abyzov@gmail.com>
2017-08-10 01:11:29 +0300

    tape equilibrium

---
+++ TapeEquilibrium/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+// you can also use other imports, for example:
+// using System.Collections.Generic;
+
+// you can write to stdout for debugging purposes, e.g.
+// Console.WriteLine("this is a debug message");
+
+class Solution
+{
+    public int solution(int[] A)
+    {
+        if (A.Length < 2)
+            return -1;
+
+        var minDiff = int.MaxValue;
+        var sum = A.Sum();
+        var currentSum = A[0];
+        for (int pivot = 1; pivot < A.Length; pivot++)
+        {
+            int leftSum = currentSum;
+            int rightSum = sum - currentSum;
+            currentSum += A[pivot];
+            minDiff = Math.Min(minDiff, Math.Abs(leftSum - rightSum));
+        }
+        return minDiff;
+    }
+
+    public static void Main()
+    {
+        var arr = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+        var result = new Solution().solution(arr);
+        Console.WriteLine(result);
+    }
+}
