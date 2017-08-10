ded8c58a0fde3659d4e299843a6461e514b42fb6
Anton <anton.abyzov@gmail.com>
2017-08-10 14:11:53 +0300

    passing cars

---
+++ PassingCars/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+// you can also use other imports, for example:
+// using System.Collections.Generic;
+
+// you can write to stdout for debugging purposes, e.g.
+// Console.WriteLine("this is a debug message");
+
+class Solution
+{
+    public int solution(int[] A)
+    {
+        if (A.Length == 0)
+            return -1;
+        long totalSum = 0;
+        
+        long sumInTheArray = A.Sum(); 
+
+        long collectedsum = 0;
+        
+        for (int i = 0; i < A.Length; i++)
+        {
+            collectedsum += A[i];
+            if (A[i] == 0)
+            {
+                totalSum += (sumInTheArray - collectedsum);
+            }
+        }
+        if (totalSum > 1000000000)
+            return -1;
+        
+        return (int) totalSum;
+    }
+
+    public static void Main()
+    {
+        var arr = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+        var result = new Solution().solution(arr);
+        Console.WriteLine(result);
+    }
+}
