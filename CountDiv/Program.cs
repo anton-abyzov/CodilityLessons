945f0144534c5dd8f492ba5b58ab60d4b5d734b7
Anton <anton.abyzov@gmail.com>
2017-08-10 14:45:37 +0300

    countdiv 25% perf

---
+++ CountDiv/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+// you can also use other imports, for example:
+// using System.Collections.Generic;
+
+// you can write to stdout for debugging purposes, e.g.
+// Console.WriteLine("this is a debug message");
+
+class Solution
+{
+    public int solution(int A, int B, int K)
+    {
+        var counter = 0;
+        var index = A;
+        while(index <=B)
+        {
+            if (index % K == 0)
+            {
+                counter++;
+                index += K;
+            }
+            else
+            {
+                index++;
+            }
+        }
+        //Console.WriteLine(counter);
+        //counter = 0;
+        //var diff = B - A;
+        //var start = (B % K == 0 ? 1 : 0);
+        //var end = (A % K == 0 ? 1 : 0);
+        //var inMiddle = diff / K;
+        //counter =  inMiddle + start + end;
+        return counter;
+    }
+
+    public static void Main()
+    {
+        var A = Convert.ToInt32(Console.ReadLine());
+        var B = Convert.ToInt32(Console.ReadLine());
+        var K = Convert.ToInt32(Console.ReadLine());
+        var result = new Solution().solution(A,B,K);
+        Console.WriteLine(result);
+    }
+}
