e4b33e1eb9b6d01d69aa98ec4ddad9d5425e17f4
Anton <anton.abyzov@gmail.com>
2017-08-10 19:28:39 +0300

    triangle

---
+++ MaxProductOfThree/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+// you can also use other imports, for example:
+// using System.Collections.Generic;
+
+// you can write to stdout for debugging purposes, e.g.
+// Console.WriteLine("this is a debug message");
+
+class Solution
+{
+    public int solution(int[] A)
+    {
+
+        Array.Sort(A);
+        var max = int.MinValue;
+        //for (int i = 2; i < A.Length; i++)
+        //{
+        var lastIndex = A.Length - 1;
+        max = Math.Max(max, A[lastIndex] * A[lastIndex - 1] * A[lastIndex - 2]);
+        max = Math.Max(max, A[0] * A[1] * A[lastIndex]);
+        //}
+
+        return max;
+    }
+
+    public static void Main()
+    {
+        var arr = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+        var result = new Solution().solution(arr);
+        Console.WriteLine(result);
+    }
+}
---
+++ Triangle/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+// you can also use other imports, for example:
+// using System.Collections.Generic;
+
+// you can write to stdout for debugging purposes, e.g.
+// Console.WriteLine("this is a debug message");
+
+class Solution
+{
+    public int solution(int[] A)
+    {
+        Array.Sort(A);
+        for (int i = 2; i < A.Length; i++)
+        {
+            if (A[i] - A[i - 1] < A[i - 2] && A[i - 1] - A[i - 2] < A[i] && A[i] - A[i - 2] < A[i - 1])
+                return 1;
+        }
+        return 0;
+    }
+
+    public static void Main()
+    {
+        var arr = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+        var result = new Solution().solution(arr);
+        Console.WriteLine(result);
+    }
+}
