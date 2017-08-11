2f39cf74d5a7584e1e21689f1416e5f9aef6732d
Anton <anton.abyzov@gmail.com>
2017-08-11 19:12:24 +0300

    task1 and task3 as I submitted it

---
+++ Task1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+// you can also use other imports, for example:
+// using System.Collections.Generic;
+
+// you can write to stdout for debugging purposes, e.g.
+// Console.WriteLine("this is a debug message");
+
+class Solution
+{
+    public bool solution(int[] A)
+    {
+
+        //Array.Sort(A, new Comparison<int>((x, y) =>
+        //{
+        //    if (y - x > 0)
+        //        counter++;
+        //    if (counter > 1)
+
+        //    return x.CompareTo(y);
+
+        //}) );
+        //return counter;
+        var N = A.Length;
+        var previousHigher = A[0];
+        var isSwapMode = false;
+        var counter = 0;
+        for (int i = 1; i < A.Length; i++)
+        {
+            if (isSwapMode)
+            {
+                if (A[i] == A[i-1])
+                    continue;
+
+                if (A[i] < previousHigher)
+                    return false;
+                    
+                isSwapMode = false;
+                counter++;
+                if (counter > 1)
+                    return false;
+
+            }
+            if (A[i] < previousHigher)
+            {
+                isSwapMode = true;
+            }
+            else
+            {
+                previousHigher = A[i];
+            }
+        }
+        return true;
+    }
+
+    public static void Main()
+    {
+        var arr = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+        var result = new Solution().solution(arr);
+        Console.WriteLine(result);
+    }
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+
+// you can also use other imports, for example:
+// using System.Collections.Generic;
+
+// you can write to stdout for debugging purposes, e.g.
+// Console.WriteLine("this is a debug message");
+
+class Solution
+{
+    public int solution(int[] A, int D)
+    {
+        var N = A.Length;
+        if (D >= N)
+            return 0;
+
+        //for (int i = 0; i < N; i++)
+        //{
+        //    var nextStone = Array.BinarySearch(A, 0);
+
+        //}
+        var counter = D - 1;
+        bool[] stones = new bool[N + 1];
+        for (int i = 0; i < A.Length; i++)
+        {
+            var nextStone = Array.BinarySearch(A, i);
+            if (nextStone < 0)
+                continue;
+            if (!stones[nextStone])
+            {
+                stones[nextStone] = true;
+                counter--;
+            }
+            if (counter == 0) return i;
+        }
+
+
+        return -1;
+    }
+
+    public static void Main()
+    {
+        var arr = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+        var D = Convert.ToInt32(Console.ReadLine());
+        var result = new Solution().solution(arr, D);
+        Console.WriteLine(result);
+    }
+}
