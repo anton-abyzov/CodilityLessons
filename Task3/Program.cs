bb1e0adb59190fef8001c32af31a008e58bd7180
Anton <anton.abyzov@gmail.com>
2017-08-12 14:40:49 +0300

    Task3 (frog jumping over the river) with tests

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
+using Xunit;
 
 // you can also use other imports, for example:
 // using System.Collections.Generic;
@@ -7,7 +10,7 @@ using System.Linq;
 // you can write to stdout for debugging purposes, e.g.
 // Console.WriteLine("this is a debug message");
 
-class Solution
+public class Solution
 {
     public int solution(int[] A, int D)
     {
@@ -15,28 +18,100 @@ class Solution
         if (D >= N)
             return 0;
 
-        //for (int i = 0; i < N; i++)
+        //var coins = Enumerable.Range(1, D).ToArray();
+        //var combinations = new int[A.Length+1];
+        //combinations[0] = 1;
+        //for (int coin = 1; coin <= coins.Length; coin++)
         //{
-        //    var nextStone = Array.BinarySearch(A, 0);
-
+        //    for (int amount = 1; amount <= A.Length; amount++)
+        //    {
+        //        if (amount >= coin)
+        //            combinations[amount] += combinations[amount - coin];
+        //    }
         //}
-        var counter = D - 1;
-        bool[] stones = new bool[N + 1];
+
+        var timeStoneDict = new SortedDictionary<int, int>();
         for (int i = 0; i < A.Length; i++)
         {
-            var nextStone = Array.BinarySearch(A, i);
-            if (nextStone < 0)
-                continue;
-            if (!stones[nextStone])
-            {
-                stones[nextStone] = true;
-                counter--;
-            }
-            if (counter == 0) return i;
+            var time = A[i];
+            timeStoneDict.Add(time, i);
         }
 
+        var stones = new bool[N];
+        for (int timeIndex = 0; timeIndex < timeStoneDict.Count; timeIndex++)
+        {
+            if (!timeStoneDict.ContainsKey(timeIndex)) continue;
+            stones[timeStoneDict[timeIndex]] = true;
+            if (CanCross(D, stones))
+            {
+                return timeIndex;
+            }
+        }
 
         return -1;
+    }
+
+    private bool CanCross(int D, bool[] stones)
+    {
+        var currentStone = -1;
+        while (currentStone < stones.Length)
+        {
+            int step;
+            for (step = 1; step <= D; step++)
+            {
+                if (step + currentStone >= stones.Length || stones[step + currentStone])
+                {
+                    currentStone = step + currentStone;
+                    break;
+                }
+            }
+            if (step > D)
+                return false;
+        }
+        return true;
+    }
+
+    [Fact]
+    public void BinarySearchTest()
+    {
+        // array should be sorted!!!!!
+        var arr = new[] {1, -1, 0, 2, 3, 4};
+        var findOne = Array.BinarySearch(arr, 1);
+        Assert.NotEqual(1, findOne);
+    }
+
+    [Fact]
+    public void Basic_test()
+    {
+        var A = new[] { 1, -1, 0, 2, 3, 5 };
+        var D = 3;
+        var result = solution(A, D);
+        Assert.Equal(2, result);
+    }
+
+    [Fact]
+    public void Basic_not_passing()
+    {
+        var A = new[] { 1, -1, 0, 2, 3, 5 };
+        var D = 1;
+        var result = solution(A, D);
+        Assert.Equal(-1, result);
+    }
+
+    [Fact]
+    public void Simple_passing_one_step()
+    {
+        var A = new[] { 1, 4, 0, 2, 3, 5 };
+        var D = 1;
+        var result = solution(A, D);
+        Assert.Equal(5, result);
+    }
+
+    [Fact]
+    public void D_greater_or_equal_N()
+    {
+        var result = solution(new[] { 1 }, 2);
+        Assert.Equal(0, result);
     }
 
     public static void Main()
